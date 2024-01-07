a1540233fafe4e4c270ff8085a21b83b744d6ca8
Raturu0 <aziz.fauzi1811@gmail.com>
2024-01-07 20:21:23 +0700

    some change

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import "package:gohealing/Developer/DeveloperPage.dart";
 import "package:flutter/material.dart";
 import "package:firebase_core/firebase_core.dart";
-import "package:gohealing/User/pages/ListWisata.dart";
 import "package:gohealing/User/pages/LoginPage/LoginPage.dart";
 import "package:gohealing/firebase_options.dart";
-
+// HASIL DARI FITUR
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
