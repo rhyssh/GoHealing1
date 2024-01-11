1b866ba7996a200f552ff85f1b1e3145efab262b
Raturu0 <aziz.fauzi1811@gmail.com>
2024-01-11 20:57:15 +0700

    add opening page fot 2 sec

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,22 @@
-import "package:flutter/material.dart";
-import "package:firebase_core/firebase_core.dart";
-import "package:gohealing/BottomConvexBarr/BottomConvexBarr.dart";
-import "package:gohealing/Developer/Langsung/DevLangsungAllOrder.dart";
-import 'package:gohealing/Developer/Langsung/DevLangsungAllSouvenir.dart';
-import 'package:gohealing/Developer/Langsung/DevLangsungAllWisata.dart';
-import 'package:gohealing/Developer/Manual/DevSouvenirPage.dart';
-import 'package:gohealing/Developer/Manual/DevWisataPage.dart';
-import "package:gohealing/TEST/WidgetTest.dart";
-import "package:gohealing/User/pages/ListWisataPage.dart";
-import "package:gohealing/User/pages/LoginPage/LoginPage.dart";
-import "package:gohealing/User/pages/Order/OrderPage.dart";
-import 'package:gohealing/User/widgets/Card/WidgetCardSouvenir.dart';
-import "package:gohealing/YangDuluMas/LoginPageReiki.dart";
-import "package:gohealing/firebase_options.dart";
-// Semisal dari Reiki menambah di baris ini
-//  INI ADA TIDAK
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:gohealing/BottomConvexBarr/BottomConvexBarr.dart';
+import 'dart:async';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+
+  // Tambahkan penundaan 2 detik sebelum memanggil runApp
+  await Future.delayed(Duration(seconds: 2), () {
+    runApp(MyApp());
+  });
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -39,3 +24,49 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import "package:flutter/material.dart";
+// import "package:firebase_core/firebase_core.dart";
+// import "package:gohealing/BottomConvexBarr/BottomConvexBarr.dart";
+// import 'dart:async';
+
+// import "package:gohealing/firebase_options.dart";
+// // Semisal dari Reiki menambah di baris ini
+// //  INI ADA TIDAK
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: BottomConvexBarr(),
+//     );
+//   }
+// }
